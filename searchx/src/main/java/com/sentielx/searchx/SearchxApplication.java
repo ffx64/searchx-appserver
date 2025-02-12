cab70c68f63d3a3ed18ba6d247edf3205256ee49
37225059+ffx64 <37225059+ffx64@users.noreply.github.com>
2025-02-12 15:46:33 -0300

    feat: initialize project spring boot #1

---
+++ searchx/src/main/java/com/sentielx/searchx/SearchxApplication.java
@@ -0,0 +1,13 @@
+package com.sentielx.searchx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchxApplication.class, args);
+	}
+
+}
