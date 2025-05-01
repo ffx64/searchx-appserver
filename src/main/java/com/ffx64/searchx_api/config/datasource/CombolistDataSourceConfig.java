b9c60a93f95e54750f1c2b2b650328f9bd2a2613
ffx64 <37225059+ffx64@users.noreply.github.com>
2025-05-01 16:48:42 -0300

    feat(config): add multi-database support for jpa repositories

---
+++ src/main/java/com/ffx64/searchx_api/config/datasource/CombolistDataSourceConfig.java
@@ -0,0 +1,55 @@
+package com.ffx64.searchx_api.config.datasource;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+    basePackages = "com.ffx64.searchx_api.repositories.combolist",
+    entityManagerFactoryRef = "combolistEntityManagerFactory",
+    transactionManagerRef = "combolistTransactionManager"
+)
+public class CombolistDataSourceConfig {
+
+    @Primary
+    @Bean(name = "combolistDataSource")
+    public DataSource combolistDataSource() {
+        return DataSourceBuilder.create()
+            .url("jdbc:postgresql://localhost:5433/searchx_combolist")
+            .username("docker")
+            .password("docker")
+            .driverClassName("org.postgresql.Driver")
+            .build();
+    }
+
+    @Primary
+    @Bean(name = "combolistEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean combolistEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("combolistDataSource") DataSource dataSource) {
+        return builder
+            .dataSource(dataSource)
+            .packages("com.ffx64.searchx_api.entities.combolist")
+            .persistenceUnit("combolist")
+            .build();
+    }
+
+    @Primary
+    @Bean(name = "combolistTransactionManager")
+    public PlatformTransactionManager combolistTransactionManager(
+            @Qualifier("combolistEntityManagerFactory") LocalContainerEntityManagerFactoryBean factoryBean) {
+        return new JpaTransactionManager(factoryBean.getObject());
+    }
+}
