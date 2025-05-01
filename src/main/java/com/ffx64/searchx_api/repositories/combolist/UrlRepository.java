d3a22fd42532c9915ab14cd1750d0f96dac0a00c
ffx64 <37225059+ffx64@users.noreply.github.com>
2025-05-01 14:30:18 -0300

    feat: create base repository structure for combolist #6

---
+++ src/main/java/com/ffx64/searchx_api/repositories/combolist/UrlRepository.java
@@ -0,0 +1,18 @@
+package com.ffx64.searchx_api.repositories.combolist;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ffx64.searchx_api.entities.combolist.UrlEntity;
+
+@Repository
+public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
+
+    List<UrlEntity> findByUserId(Long userId);
+
+    boolean existsByUserIdAndUrl(Long userId, String url);
+    
+    List<UrlEntity> findByCreatedAt(java.util.Date createdAt);
+}
---
+++ src/main/java/com/ffx64/searchx_api/repositories/combolist/UserRepository.java
@@ -0,0 +1,17 @@
+package com.ffx64.searchx_api.repositories.combolist;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ffx64.searchx_api.entities.combolist.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    List<UserEntity> findByFileId(Long fileId);
+
+    List<UserEntity> findByCreatedAt(Date createdAt);
+}
