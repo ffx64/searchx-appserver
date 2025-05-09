19a4fe5174cad4bb7aa5ce62bbea514e2d5df4fe
ffx64 <37225059+ffx64@users.noreply.github.com>
2025-05-09 16:33:42 -0300

    feat(entity): apply lombok to searchx entity #19

--- src/main/java/com/ffx64/searchx_api/entity/searchx/AgentEntity.java
+++ src/main/java/com/ffx64/searchx_api/entity/searchx/AgentEntity.java
@@ -11,11 +11,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name="agents")
 public class AgentEntity {
-
+    
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
@@ -42,61 +44,5 @@ public class AgentEntity {
     @PrePersist
     protected void onCreate() {
         createdAt = OffsetDateTime.now();
-    }
-    
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public UserEntity getUser() {
-        return user;
-    }
-
-    public void setUser(UserEntity user) {
-        this.user = user;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public OffsetDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(OffsetDateTime createdAt) {
-        this.createdAt = createdAt;
     }
 }
--- src/main/java/com/ffx64/searchx_api/entity/searchx/UserEntity.java
+++ src/main/java/com/ffx64/searchx_api/entity/searchx/UserEntity.java
@@ -9,7 +9,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name="users")
 public class UserEntity {
@@ -33,45 +35,5 @@ public class UserEntity {
     @PrePersist
     protected void onCreate() {
         createdAt = OffsetDateTime.now();
-    }
-    
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getAgentsCount() {
-        return agentsCount;
-    }
-
-    public void setAgentsCount(int agentsCount) {
-        this.agentsCount = agentsCount;
-    }
-
-    public OffsetDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(OffsetDateTime createdAt) {
-        this.createdAt = createdAt;
     }
 }
