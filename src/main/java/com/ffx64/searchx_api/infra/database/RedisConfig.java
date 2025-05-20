ff90f8488f7ff9188ef51283bcdcebd316cde8c7
ffx64 <37225059+ffx64@users.noreply.github.com>
2025-05-20 16:33:04 -0300

    feat(redis): integrate spring-data-redis to project

---
+++ src/main/java/com/ffx64/searchx_api/infra/database/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.ffx64.searchx_api.infra.database;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    private static final String REDIS_HOST = "192.168.229.131";
+    private static final int REDIS_PORT = 6379;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        var config = new RedisStandaloneConfiguration(REDIS_HOST, REDIS_PORT);
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        var template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+}
